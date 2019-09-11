7fa3b9b430b1a1509d3dfc7ec837d4286c6ccfd9
Raine Wallace <rainebeaudubs@gmail.com>
2019-09-11 02:22:01 -0400

    added simple (unfinished) header, added styled components, minimal styling

---
+++ resume/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Heading = styled.div`
+  margin: 1rem;
+`;
+
+const Name = styled.h1`
+  font-size: 5rem;
+  color: rgb(68, 0, 0);
+`;
+
+const Email = styled.h2`
+  font-size: 2.5rem;
+`;
+
+const Header = () => {
+  return (
+    <Heading>
+      <Name>Raine Wallace</Name>
+      <Email>raine.wallace.dev@gmail.com</Email>
+    </Heading>
+  )
+}
+
+export default Header;
