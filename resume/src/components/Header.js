6a401f2aae561fe8fa3af7778b6007fe7ae2e537
Raine Wallace <rainebeaudubs@gmail.com>
2019-12-11 23:08:04 -0500

    added icons to heading

--- resume/src/components/Header.js
+++ resume/src/components/Header.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { 
+  faTwitter, 
+  faLinkedin, 
+  faGithub
+} from '@fortawesome/free-brands-svg-icons'
+import { faAt } from '@fortawesome/free-solid-svg-icons';
+
 const Heading = styled.div`
   margin: 2rem;
 `;
 
 const Name = styled.h1`
   font-size: 5rem;
-  margin: 1rem;
+  margin: 2rem 1rem;
   color: rgb(68, 0, 0);
+`;
+
+const ContSec = styled.div`
+  display: flex;
+  margin-top: 1.5rem
 `;
 
 const Contact = styled.h2`
@@ -19,10 +32,23 @@ const Header = () => {
   return (
     <Heading>
       <Name>Raine Wallace</Name>
-      <Contact>raine.wallace.dev@gmail.com</Contact>
-      <Contact>RaineBeauDubs (GitHub)</Contact>
-      <Contact>@RaineWallace1 (Twitter)</Contact>
-      <Contact>raine-wallace (LinkedIn)</Contact>
+      <ContSec>
+        <FontAwesomeIcon className="icon" icon={faAt} />
+        <Contact>RaineBeauDubs@gmail.com</Contact>
+      </ContSec>
+      <ContSec>
+        <FontAwesomeIcon className="icon" icon={faGithub} />
+        <Contact>RaineBeauDubs</Contact>
+      </ContSec>
+      <ContSec>
+        <FontAwesomeIcon className="icon" icon={faTwitter} />
+        <Contact>RaineWallace1</Contact>
+      </ContSec>
+      <ContSec>
+        <FontAwesomeIcon className="icon" icon={faLinkedin} />
+        <Contact>Raine-Wallace</Contact>
+      </ContSec>
+
     </Heading>
   )
 }
