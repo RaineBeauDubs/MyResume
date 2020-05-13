19a93adc4866cb11393f9cb060555c350a302c05
Raine Wallace <rainebeaudubs@gmail.com>
2020-05-13 13:16:51 -0400

    added projects shell

---
+++ resume/src/components/Projects.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+  ResSection,
+  ResSectionTop,
+  ResSectionBottom,
+} from '../reusableStyles';
+
+
+
+
+const Projects = () => {
+  return (
+    <ResSection>
+      <ResSectionTop>Projects:</ResSectionTop>
+      <ResSectionBottom>
+      
+      </ResSectionBottom>
+    </ResSection>
+  )
+}
+
+export default Projects;
