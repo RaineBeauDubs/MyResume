4277d3cb23c388c281904141914851b4bf2ad7e3
Raine Wallace <rainebeaudubs@gmail.com>
2020-05-18 13:10:35 -0400

    replaced repeaded verbs, specified numbers

--- resume/src/components/Experience.js
+++ resume/src/components/Experience.js
@@ -52,7 +52,7 @@ const Experience = () => {
             <ExpDates>02/2020 - 05/2020</ExpDates>
           </ExpAlign>
           <ExpList>
-            <ListItems>Managed 15 Team Leads to ensure that all students received daily code reviews and sprint reviews</ListItems>
+            <ListItems>Managed 15 Team Leads to ensure that all 140+ students received daily code reviews and weekly sprint reviews</ListItems>
             <ListItems>Communicated with Team Leads and instructors, hosted daily stand-up meetings</ListItems>
             <ListItems>Coordinated with Student Success about student and Team Lead progress</ListItems>
             <ListItems>Resolved student and Team Lead conflicts</ListItems>
@@ -66,8 +66,8 @@ const Experience = () => {
           <ExpList>
             <ListItems>Graded all work from and supported a group of 8-10 students through a 2-month Computer Science curriculum that included blockchain, graphing, Python, Django, and much more</ListItems>
             <ListItems>Acted as People Manager for the performance management, professional growth, and development of each team member</ListItems>
-            <ListItems>Acted as Project Lead for multiple Build-Week projects</ListItems>
-            <ListItems>Led/hosted daily stand-up meetings with students</ListItems>
+            <ListItems>Assumed the role of Project Lead for multiple Build-Week projects</ListItems>
+            <ListItems>Directed daily stand-up meetings with students</ListItems>
           </ExpList>
         </ExpSec>
         <ExpSec>
@@ -78,9 +78,9 @@ const Experience = () => {
           <ExpList>
             <ListItems>Graded all work from and supported multiple groups of 7-10 students through a 3-month Full-Stack Web Development curriculum that included HTML/CSS, JavaScript, React, SQL, PostgreSQL, Express, and Node</ListItems>
             <ListItems>Acted as People Manager for the performance management, professional growth, and development of each team member</ListItems>
-            <ListItems>Acted as Project Lead for multiple Build-Week projects</ListItems>
-            <ListItems>Led/hosted Q&A and After Hours sessions for students</ListItems>
-            <ListItems>Led/hosted daily stand-up meetings with students</ListItems>
+            <ListItems>Assumed the role of Project Lead for multiple Build-Week projects</ListItems>
+            <ListItems>Conducted Q&A and After Hours sessions for students</ListItems>
+            <ListItems>Directed daily stand-up meetings with students</ListItems>
           </ExpList>
         </ExpSec>
         <ExpSec>
@@ -93,7 +93,7 @@ const Experience = () => {
             <ListItems>Managed, operated, and co-owned a consignment shop with 6 employees and around 600 active consignors</ListItems>
             <ListItems>Priced, tagged, and arranged hundreds of articles of clothing and accessories a week</ListItems>
             <ListItems>Refined customer service skills daily by supporting customers and consignors, personal shopping, and resolving any conflicts that came up</ListItems>
-            <ListItems>Awarded "Best Consignment" in the "Best of Oldham" competition for every year the competition has taken place (6 years and counting!)</ListItems>
+            <ListItems>Contributed to being awarded "Best Consignment" in the "Best of Oldham" competition for every year the competition has taken place (6 years and counting!)</ListItems>
           </ExpList>
         </ExpSec>
       </ResSectionBottom>
